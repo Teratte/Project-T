ef8358e5b5c699720bc15991432a195f446c0e78
zzz03000 <gustp1234567@naver.com>
2024-07-22 19:10:44 +0900

    Time Level
    시간에 따른 레벨 추가(임시)
    몬스터 체력, 스피드 추가

--- Project_T/Assets/Scripts/Enemy.cs
+++ Project_T/Assets/Scripts/Enemy.cs
@@ -5,15 +5,20 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     public float speed;
+    public float health;
+    public float maxHealth;
+    public RuntimeAnimatorController[] animCon;
     public Rigidbody2D target;
 
-    bool isLive = true;
+    bool isLive;
 
     Rigidbody2D rigid;
     SpriteRenderer spriter;
-    void Start()
+    Animator anim;
+    void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
         spriter= GetComponent<SpriteRenderer>();
     }
 
@@ -37,5 +42,36 @@ public class Enemy : MonoBehaviour
     void OnEnable()
     {
         target = GameManager.instance.player.GetComponent<Rigidbody2D>();
+        isLive = true;
+        health = maxHealth;
     }
+    public void Init(SpawnData data)
+    {
+        if (anim == null)
+        {
+            Debug.LogError("Animator is null");
+            return;
+        }
+        if (animCon == null || animCon.Length == 0)
+        {
+            Debug.LogError("animCon array is null or empty");
+            return;
+        }
+        if (data == null)
+        {
+            Debug.LogError("SpawnData is null");
+            return;
+        }
+        if (data.spriteType < 0 || data.spriteType >= animCon.Length)
+        {
+            Debug.LogError("Invalid spriteType index");
+            return;
+        }
+
+        anim.runtimeAnimatorController = animCon[data.spriteType];
+        speed = data.speed;
+        maxHealth = data.health;
+        health = data.health;
+    }
+
 }
--- Project_T/Assets/Scripts/GameManager.cs
+++ Project_T/Assets/Scripts/GameManager.cs
@@ -5,11 +5,23 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
+
+    public float gameTime; // 현재 게임 시간
+    public float maxGameTime = 2 * 10f; // 최대 게임 시간 테스트기 때문에 20초
+
     public PoolManager pool;
     public PlayerController player;
 
     void Awake()
     {
         instance = this;    
+    }
+    void Update()
+    {
+        gameTime += Time.deltaTime;
+        if (gameTime > maxGameTime)
+        {
+            gameTime = maxGameTime;
+        }
     }
 }
